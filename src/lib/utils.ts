ad5ad71c512f8cfbcdd826dd339d9747bb540328
Aman Shahid <shahidamaan2019@gmail.com>
2024-08-19 14:05:27 +0500

    updated favicon

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -17,12 +17,12 @@ export function formatDateString(dateString: string) {
   const date = new Date(dateString);
   const formattedDate = date.toLocaleDateString("en-US", options);
 
-  const time = date.toLocaleTimeString([], {
-    hour: "numeric",
-    minute: "2-digit",
-  });
+  // const time = date.toLocaleTimeString([], {
+  //   hour: "numeric",
+  //   minute: "2-digit",
+  // });
 
-  return `${formattedDate} at ${time}`;
+  return formattedDate;
 }
 
 // 
